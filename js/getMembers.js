ab6abb0cdbc50da06e7469b115d8118302c53bde
huzeifa <huzeifa@localhost.localdomain>
2014-05-28 09:44:25 +0300

    Update to website: changed color to blue and added effects

--- js/getMembers.js
+++ js/getMembers.js
@@ -17,21 +17,21 @@ $(function(){
     
     // loading spinner with ridiculous amount of options
     var opts = {
-            lines: 10, // The number of lines to draw
-            length: 100, // The length of each line
-            width: 50, // The line thickness
-            radius: 20, // The radius of the inner circle
+            lines: 12, // The number of lines to draw
+            length: 1, // The length of each line
+            width: 5, // The line thickness
+            radius: 50, // The radius of the inner circle
             corners: 1, // Corner roundness (0..1)
             rotate: 0, // The rotation offset
-            color: '#000', // #rgb or #rrggbb
+            color: '#0074D9', // #rgb or #rrggbb
             speed: 1, // Rounds per second
-            trail: 60, // Afterglow percentage
+            trail: 50, // Afterglow percentage
             shadow: false, // Whether to render a shadow
             hwaccel: false, // Whether to use hardware acceleration
             className: 'spinner', // The CSS class to assign to the spinner
             zIndex: 2e9, // The z-index (defaults to 2000000000)
             top: 25, // Top position relative to parent in px
-            left: 25 // Left position relative to parent in px
+            left: 100 // Left position relative to parent in px
         };
 	var target = document.getElementById('loading');
 	var spinner = new Spinner(opts).spin(target);
--- js/main.js
+++ js/main.js
@@ -1,6 +1,18 @@
-$(function(){
+$(function() {
+	//ScrollTo
 	$('#about').click(function(){
 		$.scrollTo('#aboutLocation', 800, {});
 	});
 	
+	//Slick.js config
+	$('.slide').slick({autoplay: true, autoplaySpeed: 8000, arrows: false, dots: true, speed: 800});
+	
+	//home-page header slide down
+	$('.jumbotron').animate({
+		height: 'show', 
+	}, 1100, 'swing');
+	
+	$('.slide').fadeIn(2000);
+	
 })
+
