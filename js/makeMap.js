f2213d9081f7aa86db1691877bf8a6e78a448f66
huzeifa <huzeifa@localhost.localdomain>
2014-06-07 22:56:24 +0300

    Maps now showing

--- js/makeMap.js
+++ js/makeMap.js
@@ -1,6 +1,6 @@
 var map = L.map('map').setView([-1.297782, 36.791982], 17);
 
-L.tileLayer('http://{s}.tile.cloudmade.com/2515f952e6c3418488627c3e24cd773b/997/256/{z}/{x}/{y}.png', {
+L.tileLayer('http://{s}.tile.osm.org/{z}/{x}/{y}.png', {
     attribution: 'Map data &copy; <a href="http://openstreetmap.org">OpenStreetMap</a> contributors, <a href="http://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="http://cloudmade.com">CloudMade</a>',
     maxZoom: 18
 }).addTo(map);
