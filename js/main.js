f6f58ac16043741c30462e174e8a09ca18fd2966
huzeifa <huzeifa@localhost.localdomain>
2014-06-14 18:11:41 +0300

    Removed slide down effect

--- js/main.js
+++ js/main.js
@@ -4,12 +4,5 @@ $(function() {
 		$.scrollTo('#aboutLocation', 800, {});
 	});
 	
-	//home-page header slide down
-	$('.jumbotron').animate({
-		height: 'show', 
-	}, 1100, 'swing');
-	
-	$('.slide').fadeIn(2000);
-	
 })
 
