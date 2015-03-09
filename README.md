a72bc8089daf0c54c7fc8cb9ac025214ded52b79
Eugene Mutai <eugenemutai@gmail.com>
2015-03-09 19:43:42 +0300

    the new face of nairobi.js, should hold for now

--- README.md
+++
@@ -1,6 +0,0 @@
-NairobiJS.github.io
-===================
-
-static webpage for the Nairobi javascript user group
-
-Trello to-do list: https://trello.com/b/lpj8bRBM/nairobijs-github-io
--- js/makeCalendar.js
+++
@@ -1,7 +0,0 @@
-$(function(){
-	$('#calendar').fullCalendar({
-		events: 'https://www.google.com/calendar/feeds/uulroen48rg3fr1rj1pstm8jd0%40group.calendar.google.com/public/basic',
-		className: 'event',	// CSS class to attach to each event
-		height: 400
-	});
-})
